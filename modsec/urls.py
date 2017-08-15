0b9853576e4bb34a0f9ba59f6002c0e0add59aac
Simon Egea <simon.egea48@gmail.com>
2017-08-15 13:16:20 +0200

    Realizadas las vistas para mostrar las reglas y los registros

--- modsec/urls.py
+++ modsec/urls.py
@@ -4,5 +4,14 @@ from . import views
 
 
 urlpatterns = [
+    #Vista principal
     url(r'^$', views.index, name='index'),
+    #Reglas
+    url(r'^rules/', views.rules, name='rules'),
+    #Registros
+    url(r'^logs/', views.logs, name='logs'),
+    #Mostrar una regla
+    url(r'^showRule/(?P<rule_id>[0-9]+)/$', views.showRule, name='showRule'),
+    #Mostrar un registro
+    url(r'^showLog/(?P<log_id>[0-9]+)/$', views.showLog, name='showLog'),
     ]
--- modsec/views.py
+++ modsec/views.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from modsec.models import Log,Rule
+from django.http import Http404
 # Create your views here.
 def index(request):
     ultimos_logs = Log.objects.order_by('-date')[:5]
@@ -11,3 +12,52 @@ def index(request):
     aux ={'reglas_recientes': reglas_recientes,'ultimos_logs':ultimos_logs }
     
     return render(request, 'modsec/index.html', aux)
+
+def rules(request):
+    reglas=Rule.objects.all()
+    paginator = Paginator(reglas, 10) # Show 25 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        reglas = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        reglas = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        reglas = paginator.page(paginator.num_pages)
+
+    context={'reglas': reglas}
+    return render(request, 'modsec/rules.html',context)
+
+def logs(request):
+    registros=Log.objects.all()
+    paginator = Paginator(registros, 10) # Show 25 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        registros = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        registros = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        registros = paginator.page(paginator.num_pages)
+
+    context={'registros': registros}
+    return render(request, 'modsec/logs.html',context)
+
+def showRule(request,rule_id):
+    try:
+        rule = Rule.objects.get(pk=rule_id)
+    except Rule.DoesNotExist:
+        raise Http404("Rule does not exist")
+    return render(request, 'modsec/displayRule.html', {'rule': rule})
+
+def showLog(request,log_id):
+    try:
+        log = Log.objects.get(pk=log_id)
+    except Log.DoesNotExist:
+        raise Http404("Log does not exist")
+    return render(request, 'modsec/displayLog.html', {'log': log})
+
